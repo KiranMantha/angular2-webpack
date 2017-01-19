a87220f964f9216e4e9a066b62c693bb446cf9b6
Rambabu Chimata <kmantha@loyaltymethods.com>
2017-01-19 11:55:07 +0530

    Added sass support

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -4,6 +4,7 @@ const HtmlWebpack = require('html-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 const ChunkWebpack = webpack.optimize.CommonsChunkPlugin;
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const rootDir = path.resolve(__dirname, '..');
 
@@ -12,17 +13,22 @@ module.exports = {
     devServer: {
         contentBase: path.resolve(rootDir, 'dist'),
         port: 9100,
-        hot: true
+        hot: true 
     },
     devtool: 'source-map',
     entry: {
         app: [ path.resolve(rootDir, 'src', 'bootstrap') ],
-        vendor: [ path.resolve(rootDir, 'src', 'vendor') ]
+        vendor: [ path.resolve(rootDir, 'src', 'vendor') ],
+        style: [ path.resolve(rootDir, 'src/Styles', 'app') ]
     },
     module: {
         loaders: [
             { loader: 'raw', test: /\.(css|html)$/ },
-            { loader: ['style-loader', 'css-loader', 'sass-loader'], test: /\.scss$/ },
+            { test: /\.scss$/, loader: ExtractTextPlugin.extract(
+                    'style', // backup loader when not building .css file
+                    'css!sass' // loaders to preprocess CSS
+                ) 
+            },
             { exclude: /node_modules/, loader: 'ts', test: /\.ts$/ }
         ]
     },
@@ -40,9 +46,10 @@ module.exports = {
             filename: 'index.html',
             inject: 'body',
             template: path.resolve(rootDir, 'src', 'index.html')
-        })
+        }),
+        new ExtractTextPlugin('[name].css')
     ],
     resolve: {
-        extensions: [ '', '.js', '.ts' ]
+        extensions: [ '', '.js', '.ts', '.scss' ]
     }
 };
